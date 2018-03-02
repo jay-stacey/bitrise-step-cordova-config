884e5b09ae0a99dfb10075630c5e2eb0353835b8
Steven <steven.gunneweg@aanzee.nl>
2018-03-02 09:33:07 +0100

    Added setting of config version

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const CONFIG_FILE_PATH = process.env['config_path'] || 'config.xml'
 
 const NAME = process.env['name'];
 const ID = process.env['id'];
+const VERSION = process.env['version'];
 
 const ANDROID_VERSION_CODE = process.env['android_version_code'];
 const ANDROID_PACKAGE_NAME = process.env['android_package_name'];
@@ -27,6 +28,12 @@ if(ID) {
   config.setID(ID);
 
   log('id', ID);
+}
+
+if(VERSION) {
+  config.setVersion(VERSION);
+
+  log('version', VERSION);
 }
 
 if (ANDROID_VERSION_CODE) {
