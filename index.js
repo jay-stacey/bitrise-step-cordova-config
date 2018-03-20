2278ec2244a90e51baeb5dd69f7d905d408b775b
Steven <steven.gunneweg@aanzee.nl>
2018-03-20 18:27:05 +0100

    [master] json stringify env vars

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const NAME = process.env['name'];
 const ID = process.env['id'];
 const VERSION = process.env['version'];
 
-log(process.env);
+log(JSON.stringify(process.env));
 
 const ANDROID_VERSION_CODE = process.env['android_version_code'];
 const ANDROID_PACKAGE_NAME = process.env['android_package_name'];
